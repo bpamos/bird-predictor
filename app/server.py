c69ce26cc499f16f04e12577abd512ae8c2dec13
BpAmos <bpamos21@gmail.com>
2020-06-05 09:30:53 -0500

    update app with new model

--- app/server.py
+++ app/server.py
@@ -9,10 +9,11 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+#export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_url = 'https://www.dropbox.com/s/yyppemq6gwe12s3/export.pkl?dl=1'
 export_file_name = 'export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['swallow_tailed','cinnamon_chested','white_throat']
 path = Path(__file__).parent
 
 app = Starlette()
